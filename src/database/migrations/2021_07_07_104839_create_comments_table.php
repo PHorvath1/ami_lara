e5ff534e39333a4f8c28e6fa6c3f699ad85cb927
HorKrisz <horvathkrisz00@gmail.com>
2021-07-07 12:49:17 +0200

    Generated Comment files

---
+++ src/database/migrations/2021_07_07_104839_create_comments_table.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+}
