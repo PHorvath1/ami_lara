688c12f9556ffdf81703877b1e3893599ebc0c9d
Dombi Tibor <40638464+dombidav@users.noreply.github.com>
2021-07-07 10:48:11 +0200

    Implemented ArticleRequest file uploader

--- src/app/Http/Requests/ArticleRequest.php
+++ src/app/Http/Requests/ArticleRequest.php
@@ -2,19 +2,46 @@
 
 namespace App\Http\Requests;
 
-use Illuminate\Foundation\Http\FormRequest;
+use App\Exceptions\EmptyRequestException;
+use Illuminate\Support\Str;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
-class ArticleRequest extends FormRequest
+class ArticleRequest extends ExtendedFormRequest
 {
-    public function authorize(): bool
-    {
-        return true;
-    }
-
     public function rules(): array
     {
         return [
-            //
+            'pdf' => ['file'] //TODO: complete implementation
         ];
+    }
+
+    /**
+     * Upload a file
+     * @throws EmptyRequestException $request is empty
+     * @throws BadRequestHttpException if request doesn't have input named $fileInputName
+     * @throws FileException if file couldn't be moved or created
+     * @returns string|bool Path of uploaded file or false on fail
+     */
+    public function upload(
+        ArticleRequest $request,
+        string $fileInputName,
+        string $path = '',
+        $throwErrorOnMissingFile = false
+    ): string|bool {
+        if (!$request) throw new EmptyRequestException(ArticleRequest::class, __FILE__, __LINE__, __FUNCTION__);
+        $hasFile = $request->hasFile($fileInputName);
+
+        if (!$hasFile && $throwErrorOnMissingFile) throw new BadRequestHttpException("Request does not have input file named $fileInputName"
+        );
+
+        if ($hasFile) {
+            $uploadedFile = $request->file($fileInputName);
+            return $uploadedFile->move("uploads/$path",
+                                       Str::random(8) . '_' . $uploadedFile->getClientOriginalExtension()
+            )->getFilename();
+        }
+
+        return false;
     }
 }
