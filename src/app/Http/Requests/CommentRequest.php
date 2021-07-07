d7f423344949962acf015915a5fe36a46a2b301b
HorKrisz <horvathkrisz00@gmail.com>
2021-07-07 13:00:43 +0200

    Implemented Comment request

--- src/app/Http/Requests/CommentRequest.php
+++ src/app/Http/Requests/CommentRequest.php
@@ -2,19 +2,13 @@
 
 namespace App\Http\Requests;
 
-use Illuminate\Foundation\Http\FormRequest;
-
-class CommentRequest extends FormRequest
+class CommentRequest extends ExtendedFormRequest
 {
-    public function authorize(): bool
-    {
-        return true;
-    }
 
     public function rules(): array
     {
         return [
-            //
+            'content' => [ 'required', 'min:10' ]
         ];
     }
 }
