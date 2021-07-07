1b6d6ff31a2956bb066c321d145d0fb91996deb3
HorKrisz <horvathkrisz00@gmail.com>
2021-07-07 13:50:43 +0200

    Documented Article model

--- src/app/Models/Article.php
+++ src/app/Models/Article.php
@@ -11,9 +11,13 @@ use Illuminate\Database\Eloquent\Model;
  * Class Article
  * @package App\Models
  *
- * @property string $id
- * @property DateTime $created_at
- * @property DateTime $updated_at
+ * @property string id
+ * @property string name
+ * @property string summary
+ * @property int state
+ * @property DateTime deleted_at
+ * @property DateTime created_at
+ * @property DateTime updated_at
  */
 class Article extends Model
 {
