e8a1aee36887e0177422f5495901674c786484d9
Dombi Tibor <40638464+dombidav@users.noreply.github.com>
2021-07-07 15:40:49 +0200

    Implemented StatusText attribute

--- src/app/Models/Article.php
+++ src/app/Models/Article.php
@@ -6,6 +6,8 @@ use App\Traits\ApiResource;
 use DateTime;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Str;
+use Symfony\Component\Routing\Exception\InvalidParameterException;;
 
 /**
  * Class Article
@@ -22,4 +24,27 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model
 {
     use HasFactory, ApiResource;
+
+    private const STATES = [ 'SUBMITTED', 'UNDER_REVIEW', 'ACCEPTED', 'REJECTED' ];
+
+    protected $appends = ['stateText'];
+
+    public function getStateTextAttribute(){
+        return $this->state > count(self::STATES) || $this->state < 0
+            ? 'UNKNOWN'
+            : self::STATES[$this->state];
+
+    }
+
+    /**
+     * @throws InvalidParameterException If state text is not found in STATES
+     */
+    public function setStateTextAttribute($value){
+        $value = Str::of($value)->replace(' ', '_')->upper();
+        $index = array_search($value, self::STATES, false);
+        if ($index < 0) throw new InvalidParameterException('Unknown State');
+
+        $this->state = $index;
+        $this->save();
+    }
 }
