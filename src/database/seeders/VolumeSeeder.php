35d5bc3a81edf220c45061da140f32afa1e5b712
HorKrisz <horvathkrisz00@gmail.com>
2021-07-09 11:08:46 +0200

    Implemented Volume seeder

--- src/database/seeders/VolumeSeeder.php
+++ src/database/seeders/VolumeSeeder.php
@@ -2,12 +2,18 @@
 
 namespace Database\Seeders;
 
+use App\Models\Volume;
 use Illuminate\Database\Seeder;
 
 class VolumeSeeder extends Seeder
 {
     public function run(): void
     {
-        //
+        for ($i = 0; $i < 30; $i++) {
+            /** @var Volume $volume */
+            $volume = Volume::factory()->make();
+            $volume->release_year = random_int(2000, 2022);
+            $volume->save();
+        }
     }
 }
