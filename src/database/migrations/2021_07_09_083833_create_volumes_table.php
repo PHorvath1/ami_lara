5cd2cf49b9285aec8779a0f22a3c53834a010df9
HorKrisz <horvathkrisz00@gmail.com>
2021-07-09 10:40:10 +0200

    Generated Volume files

---
+++ src/database/migrations/2021_07_09_083833_create_volumes_table.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVolumesTable extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('volumes', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('volumes');
+    }
+}
