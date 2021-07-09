a42aaf24564e09ffb453d49bb9cea93f82e28151
HorKrisz <horvathkrisz00@gmail.com>
2021-07-09 10:50:32 +0200

    Documented Volume model

--- src/app/Models/Volume.php
+++ src/app/Models/Volume.php
@@ -3,9 +3,19 @@
 namespace App\Models;
 
 use App\Traits\ApiResource;
+use DateTime;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Volume
+ * @package App\Models
+ *
+ * @property int id
+ * @property int release_year
+ * @property DateTime created_at
+ * @property DateTime updated_at
+ */
 class Volume extends Model
 {
     use HasFactory, ApiResource;
