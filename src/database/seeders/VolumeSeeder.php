a9d7c0deb7cc15ab99046989df88a56f56fce8d3
HorKrisz <horvathkrisz00@gmail.com>
2021-07-09 12:29:48 +0200

    Changed for loop to factory create in Volume seeder

--- src/database/seeders/VolumeSeeder.php
+++ src/database/seeders/VolumeSeeder.php
@@ -9,11 +9,6 @@ class VolumeSeeder extends Seeder
 {
     public function run(): void
     {
-        for ($i = 0; $i < 30; $i++) {
-            /** @var Volume $volume */
-            $volume = Volume::factory()->make();
-            $volume->release_year = random_int(2000, 2022);
-            $volume->save();
-        }
+        Volume::factory(30)->create();
     }
 }
