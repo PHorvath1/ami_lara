1d68704c285a803b29afc074595eb46285ad1205
HorKrisz <horvathkrisz00@gmail.com>
2021-07-12 08:46:56 +0200

    Implemented Category seeder

--- src/database/seeders/CategorySeeder.php
+++ src/database/seeders/CategorySeeder.php
@@ -2,12 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class CategorySeeder extends Seeder
 {
     public function run(): void
     {
-        //
+        Category::factory(10)->create();
     }
 }
