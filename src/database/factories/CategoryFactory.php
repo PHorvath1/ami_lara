96e60ed83c5ea2fdec5ab36d9745ea462fe49168
HorKrisz <horvathkrisz00@gmail.com>
2021-07-12 08:43:20 +0200

    Implemented Category factory

--- src/database/factories/CategoryFactory.php
+++ src/database/factories/CategoryFactory.php
@@ -12,7 +12,7 @@ class CategoryFactory extends Factory
     public function definition() : array
     {
         return [
-            //
+            'name' => $this->faker->sentence(1)
         ];
     }
 }
