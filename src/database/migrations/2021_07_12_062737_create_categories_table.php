00674fe5136433d88975d75c11c5691456e21010
HorKrisz <horvathkrisz00@gmail.com>
2021-07-12 08:31:39 +0200

    Implemented Category migration

--- src/database/migrations/2021_07_12_062737_create_categories_table.php
+++ src/database/migrations/2021_07_12_062737_create_categories_table.php
@@ -10,7 +10,7 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('name');
         });
     }
 
