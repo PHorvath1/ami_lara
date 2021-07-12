b891409c0d24d9477ae7bf4fc55c968eb7f78654
HorKrisz <horvathkrisz00@gmail.com>
2021-07-12 08:44:43 +0200

    Documented Category model

--- src/app/Models/Category.php
+++ src/app/Models/Category.php
@@ -6,6 +6,13 @@ use App\Traits\ApiResource;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Category
+ * @package App\Models
+ *
+ * @property int id
+ * @property string name
+ */
 class Category extends Model
 {
     use HasFactory, ApiResource;
