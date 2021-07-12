2adaf28baf441e9042fb93a83bd39332dd10bdc5
HorKrisz <horvathkrisz00@gmail.com>
2021-07-12 08:30:30 +0200

    Generated Category files

---
+++ src/app/Models/Category.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\ApiResource;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory, ApiResource;
+}
