822ea622d6a41ea0e310798ca9007a01b18daf18
PHorvath1 <hpeti291@gmail.com>
2021-07-12 09:46:30 +0200

    Implemented Tag seeder

---
+++ src/database/seeders/TagSeeder.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Tag;
+use Illuminate\Database\Seeder;
+
+class TagSeeder extends Seeder
+{
+    public function run(): void
+    {
+        Tag::factory(30)->create();
+    }
+}
