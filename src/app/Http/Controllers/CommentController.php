ce151252c9f9d584c582543def36330e0e8dae63
Dombi Tibor <40638464+dombidav@users.noreply.github.com>
2021-07-13 14:21:12 +0200

    Changed parent of CommentController to GuardedController

--- src/app/Http/Controllers/CommentController.php
+++ src/app/Http/Controllers/CommentController.php
@@ -11,35 +11,22 @@ use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Support\Facades\Auth;
 
-class CommentController extends Controller
+class CommentController extends GuardedController
 {
 
     public function index(): Factory|View|Application|RedirectResponse
     {
-        if(!Bouncer::can('index', Comment::class)){
-                    abort(StatusCode::FORBIDDEN);
-        }
-
         return view('comment.index', ['comments' => Comment::all()]);
     }
 
     public function create(): Factory|View|Application|RedirectResponse
     {
-         if(!Bouncer::can('create', Comment::class)){
-            abort(StatusCode::FORBIDDEN);
-         }
-
         return view('comments.form');
     }
 
     public function store(CommentRequest $request): Factory|View|Application|RedirectResponse
     {
-        if(!Bouncer::can('create', Comment::class)){
-           abort(StatusCode::FORBIDDEN);
-        }
-
        $comment = Comment::create($request->validated());
         Toastr::success('New comment created');
         return redirect(route('comments.show', [$comment]));
@@ -47,40 +34,23 @@ class CommentController extends Controller
 
     public function show(Comment $comment): Factory|View|Application|RedirectResponse
     {
-        if(!Bouncer::can('view', Comment::class)){
-            abort(StatusCode::FORBIDDEN);
-        }
         return view('comments.show', ['comment' => $comment]);
     }
 
     public function edit(Comment $comment): Factory|View|Application|RedirectResponse
     {
-        if(!Bouncer::can('edit', Comment::class)){
-            abort(StatusCode::FORBIDDEN);
-        }
-
         return view('comments.form', ['comment' => $comment]);
     }
 
     public function update(CommentRequest $request, Comment $comment): Factory|View|Application|RedirectResponse
     {
-        if(!Bouncer::can('edit', Comment::class)){
-            abort(StatusCode::FORBIDDEN);
-        }
-
         $comment->update($request->validated());
-        $comment->save();
-
         Toastr::success('Comment modified');
         return redirect(route('comments.show', [$comment]));
     }
 
     public function destroy(Comment $comment): Factory|View|Application|RedirectResponse
     {
-        if(!Bouncer::can('delete', Comment::class)){
-           abort(StatusCode::FORBIDDEN);
-        }
-
         Toastr::warning("Comment deleted: $comment->id");
         $comment->delete();
         return redirect(route('comments.index'));
