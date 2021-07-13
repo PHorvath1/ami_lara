4450aea3bf740c860cb8e15bbdd02f70a3ad474d
Dombi Tibor <40638464+dombidav@users.noreply.github.com>
2021-07-13 13:50:45 +0200

    Fixed bouncer checks in ArticleController

--- src/app/Http/Controllers/ArticleController.php
+++ src/app/Http/Controllers/ArticleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Middleware\BouncerCheck;
 use App\Http\Requests\ArticleRequest;
 use App\Models\Article;
 use App\Utils\Bouncer;
@@ -14,31 +15,23 @@ use Illuminate\Http\RedirectResponse;
 
 class ArticleController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware([BouncerCheck::class]);
+    }
 
     public function index(): Factory|View|Application|RedirectResponse
     {
-        if (!Bouncer::can('index', Article::class)) {
-            abort(StatusCode::FORBIDDEN);
-        }
-
         return view('pages.articles.index', ['articles' => Article::all()]);
     }
 
     public function create(): Factory|View|Application|RedirectResponse
     {
-        if (!Bouncer::can('create', Article::class)) {
-            abort(StatusCode::FORBIDDEN);
-        }
-
         return view('pages.articles.form');
     }
 
     public function store(ArticleRequest $request): Factory|View|Application|RedirectResponse
     {
-        if (!Bouncer::can('create', Article::class)) {
-            abort(StatusCode::FORBIDDEN);
-        }
-
         $article = Article::create($request->validated());
         Toastr::success('New article created');
         return redirect(route('articles.show', [$article]));
@@ -46,40 +39,23 @@ class ArticleController extends Controller
 
     public function show(Article $article): Factory|View|Application|RedirectResponse
     {
-        if (!Bouncer::can('view', Article::class)) {
-            abort(StatusCode::FORBIDDEN);
-        }
         return view('pages.articles.show', ['article' => $article]);
     }
 
     public function edit(Article $article): Factory|View|Application|RedirectResponse
     {
-        if (!Bouncer::can('edit', Article::class)) {
-            abort(StatusCode::FORBIDDEN);
-        }
-
         return view('pages.articles.form', ['article' => $article]);
     }
 
     public function update(ArticleRequest $request, Article $article): Factory|View|Application|RedirectResponse
     {
-        if (!Bouncer::can('edit', Article::class)) {
-            abort(StatusCode::FORBIDDEN);
-        }
-
         $article->update($request->validated());
-        $article->save();
-
         Toastr::success('Article modified');
         return redirect(route('articles.show', [$article]));
     }
 
     public function destroy(Article $article): Factory|View|Application|RedirectResponse
     {
-        if (!Bouncer::can('delete', Article::class)) {
-            abort(StatusCode::FORBIDDEN);
-        }
-
         Toastr::warning("Article deleted: $article->id");
         $article->delete();
         return redirect(route('articles.index'));
