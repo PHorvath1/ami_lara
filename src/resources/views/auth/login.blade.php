3e04ff12381e4737de06f1669c0e7dcfcc5849ca
Csathó Zsolt <csatho.zsolt@gmail.com>
2021-07-14 10:27:38 +0200

    Login page color design

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+        <div class="col-md-6">
+            <div class="card" id="login-body">
+                <div class="card-header" id="login-header">{{ __('Login') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
