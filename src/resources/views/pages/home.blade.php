5d77c539dde84268293c8a4e4a4c6edbd75e29b7
Csathó Zsolt <csatho.zsolt@gmail.com>
2021-07-14 13:30:11 +0200

    Auto stash before rebase of "origin/master"

--- src/resources/views/pages/home.blade.php
+++ src/resources/views/pages/home.blade.php
@@ -3,11 +3,11 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+        <div class="col-md-6">
+            <div class="card" id="login-body">
+                <div class="card-header mb-2" id="login-header">Dashboard</div>
 
-                <div class="card-body">
+                <div class="card-body mb-2">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
