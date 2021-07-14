39eabf0eb98c3d0df126a78e8979474263a30c1d
Vincze Flórián <vinczef.o@gmail.com>
2021-07-14 14:26:22 +0200

    Footer height lowered

--- src/resources/views/components/footer.blade.php
+++ src/resources/views/components/footer.blade.php
@@ -1,22 +1,24 @@
-<div class=" text-center text-lg-start">
-    <div class="text-center p-3">
+
+
 
 
         <footer class="d-lg-flex align-items-xl-end" style="margin-top: auto;width: 99%;padding-top: 15px;">
-            <div class="col" style="width: 100%;">
+            <div class="col flex-column" style="width: 100%;">
                 <div class="row">
-                    <div class="col d-md-flex justify-content-md-center align-items-md-center"><img src="{{ asset('img/footer/Clarivate.png') }}" style="width: 60%;"></div>
-                    <div class="col d-md-flex justify-content-md-center align-items-md-center"><img src="{{ asset('img/footer/Scopus.png') }}" style="width: 50%;"></div>
-                    <div class="col d-md-flex justify-content-md-center align-items-md-center"><img src="{{ asset('img/footer/zbMath.png') }}" style="width: 80%;"></div>
-                    <div class="col d-md-flex justify-content-md-center align-items-md-center"><img src="{{ asset('img/footer/mathscinet.png') }}"  style="width: 100%;"></div>
+                    <div class="col d-flex d-md-flex justify-content-center align-items-center justify-content-md-center align-items-md-center" style="padding-bottom: 10px;"><img src="{{ asset('img/footer/Clarivate.png') }}" style="width: 30%;" /></div>
+                    <div class="col d-flex d-md-flex justify-content-center align-items-center justify-content-md-center align-items-md-center" style="padding-bottom: 10px;"><img src="{{ asset('img/footer/Scopus.png') }}" style="width: 20%;" /></div>
+                    <div class="col d-flex d-md-flex justify-content-center align-items-center justify-content-md-center align-items-md-center" style="padding-bottom: 10px;"><img src="{{ asset('img/footer/zbMath.png') }}" style="width: 50%;" /></div>
+                    <div class="col d-flex d-md-flex justify-content-center align-items-center justify-content-md-center align-items-md-center" style="padding-bottom: 10px;"><img src="{{ asset('img/footer/mathscinet.png') }}" style="width: 50%;" /></div>
                 </div>
                 <div class="row">
-                    <div class="col d-md-flex justify-content-md-center align-items-md-center"><img src="{{ asset('img/footer/abscohost.png') }}"  style="width: 80%;"></div>
-                    <div class="col d-md-flex justify-content-md-center align-items-md-center"><img src="{{ asset('img/footer/MathEduclogo.gif') }}" style="width: 100%;"></div>
-                    <div class="col"><img src="{{ asset('img/footer/Eszterházy.png') }}" style="height: auto;width: 100%;"></div>
+                    <div class="col d-flex d-md-flex justify-content-center align-items-center justify-content-md-end align-items-md-center" style="padding-top: 10px;padding-bottom: 10px;"><img src="{{ asset('img/footer/abscohost.png') }}" style="width: 50%;margin-top: -30px;margin-right: -10px;" /></div>
+                    <div class="col d-flex d-md-flex justify-content-center align-items-center justify-content-md-center align-items-md-center" style="padding-top: 10px;padding-bottom: 10px;"><img src="{{ asset('img/footer/MathEduclogo.gif') }}" style="width: 50%;margin-top: -25px;" /></div>
+                    <div class="col d-flex d-md-flex justify-content-center align-items-center justify-content-md-start align-items-md-center" style="padding-top: 10px;padding-bottom: 10px;"><img class="d-flex justify-content-center align-items-center" src="{{ asset('img/footer/Eszterházy.png') }}" style="height: auto;width: 30%;margin-left: 38px;margin-top: -35px;" /></div>
                 </div>
             </div>
         </footer>
 
-    </div>
-</div>
+
+
+
+
