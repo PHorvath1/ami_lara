f29949cfe05708c0c9a8b7774906716193df90d3
PHorvath1 <hpeti291@gmail.com>
2021-07-14 13:25:05 +0200

    Fixed function names in Bouncer Controller

--- src/app/Http/Controllers/BouncerController.php
+++ src/app/Http/Controllers/BouncerController.php
@@ -9,13 +9,13 @@ use App\Models\User;
 class BouncerController extends Controller
 {
 
-    public function assignRole(User $user, string $role) {
+    public function assign_role(User $user, string $role) {
         $user->assign($role);
         $user->save();
         return view('pages.users.show', [$user]);
     }
 
-    public function unassignRole(User $user, string $role) {
+    public function unassign_role(User $user, string $role) {
         $user->retract($role);
         $user->save();
         return view('pages.users.show', [$user]);
