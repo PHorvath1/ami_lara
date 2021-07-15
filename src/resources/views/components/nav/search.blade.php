42d90b6158fdee1461f6401f5c8c1d5ea861ae5e
Vincze Flórián <vinczef.o@gmail.com>
2021-07-15 10:28:05 +0200

    Nav search id to form

--- src/resources/views/components/nav/search.blade.php
+++ src/resources/views/components/nav/search.blade.php
@@ -1,5 +1,5 @@
 
-            <form class="d-lg-flex me-auto justify-content-lg-end search-form" target="_self">
+            <form id="searchform" class="d-lg-flex me-auto justify-content-lg-end search-form" target="_self">
                 <div class="d-flex justify-content-lg-end"><input class="border rounded form-control search-field" type="search" id="search-field" name="search" >
                     <button id="searchbtn" class="btn btn-primary d-flex d-sm-flex d-md-flex d-lg-flex d-xl-flex justify-content-center align-items-center justify-content-sm-center align-items-sm-center justify-content-md-center align-items-md-center justify-content-lg-center align-items-lg-center justify-content-xl-center align-items-xl-center" type="button" ><i class="fas fa-search" id="searchbtnico" ></i></button></div>
             </form>
