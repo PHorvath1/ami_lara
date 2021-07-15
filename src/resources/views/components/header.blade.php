2d711b23fee09aa444104c91c221110516661007
PHorvath1 <hpeti291@gmail.com>
2021-07-15 10:31:53 +0200

    Modified header blade file

--- src/resources/views/components/header.blade.php
+++ src/resources/views/components/header.blade.php
@@ -3,9 +3,13 @@
         <div class="col" id="col1"><img src="{{asset('img/header/summa.png')}}" id="img1"></div>
         <div class="col" id="col2">
             <div class="row" id="row1">
-                <div class="col d-flex d-sm-flex d-md-flex justify-content-end align-items-center justify-content-sm-end align-items-sm-center justify-content-md-end align-items-md-center" id="col3"><img class="d-lg-flex d-xl-flex" src="{{asset('img/header/ami_logo.png')}}" id="img2"></div>
+                <div class="col d-flex d-sm-flex d-md-flex justify-content-start align-items-center justify-content-sm-start align-items-sm-center justify-content-md-start align-items-md-center" id="col3"><span class="name">Annales Mathematicae</span></div>
             </div>
-            <div class="row" id="row2"></div>
         </div>
+            <div class="row" id="row2">
+                <div class="col" id="col3">
+                    <span class="et">et</span><span class="name">Informaticae</span>
+                </div>
+            </div>
     </div>
 </header>
