76829df04cab278b78bc64a7d6626d039dd40d2d
Vincze Flórián <vinczef.o@gmail.com>
2021-07-15 10:27:24 +0200

    navbar.blde style to sass

--- src/resources/views/components/navbar.blade.php
+++ src/resources/views/components/navbar.blade.php
@@ -1,8 +1,8 @@
 
-<nav class="navbar navbar-light navbar-expand-md navigation-clean-search" style="background: rgb(249, 250, 177);width: auto;">
+<nav class="navbar navbar-light navbar-expand-md navigation-clean-search" id="navclass">
     <div class="container"><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
-        <div class="collapse navbar-collapse d-lg-flex align-content-start justify-content-lg-center" id="navcol-1" style="margin-left: 0px;">
-            <ul class="navbar-nav d-lg-flex" style="margin: 5px;padding-right: 30px;">
+        <div class="collapse navbar-collapse d-lg-flex align-content-start justify-content-lg-center" id="navcol-1" >
+            <ul class="navbar-nav d-lg-flex align-content-start justify-content-lg-center" id="navul">
 
                 <x-nav.link class="notlastlink" to="home"  />
 
@@ -14,13 +14,13 @@
 
             </ul>
         </div>
-             <div class="d-flex">
+             <div id="divdflexcontainer" class="d-flex">
+
                  <x-nav.search />
 
 
-                 <div class="btn btn-primary d-flex d-sm-flex d-xl-flex justify-content-center align-items-center justify-content-sm-center align-items-sm-center justify-content-xl-center align-items-xl-center">
-                                     <x-nav.auth />
-                                 </div>
+                 <x-nav.auth />
+
 
          </div>
     </div>
