919f77b68644c5945961fc739084bec180b14789
Dombi Tibor <40638464+dombidav@users.noreply.github.com>
2021-07-15 13:20:34 +0200

    Changed Content Route to Volume Show

--- src/app/Http/Controllers/StaticController.php
+++ src/app/Http/Controllers/StaticController.php
@@ -8,12 +8,7 @@ use Illuminate\Contracts\View\View;
 
 class StaticController extends Controller
 {
-    public function Welcome(): Factory|View|Application
-    {
-        return view('pages.welcome');
-    }
-
-    public function Home(): Factory|View|Application
+    public function home(): Factory|View|Application
     {
         return view('pages.home');
     }
@@ -21,15 +16,15 @@ class StaticController extends Controller
     public function test(){
         return view('pages.test');
     }
-    public function About(): Factory|View|Application
+    public function about(): Factory|View|Application
     {
         return view('pages.about');
     }
-    public function Content(): Factory|View|Application
+    public function content(): Factory|View|Application
     {
         return view('pages.content');
     }
-    public function Submissions(): Factory|View|Application
+    public function submissions(): Factory|View|Application
     {
         return view('pages.submissions');
     }
