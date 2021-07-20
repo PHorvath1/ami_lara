07f47132067e18ebda7f63ec89992e1883b8e6fc
Vincze Flórián <vinczef.o@gmail.com>
2021-07-20 11:54:53 +0200

    article show blade content design

--- src/resources/views/pages/articles/show.blade.php
+++ src/resources/views/pages/articles/show.blade.php
@@ -15,38 +15,33 @@
             <div class="card-body">
                 <h4>Summary: {{ $article->summary }}</h4>
             </div>
-            <div class="col">
-                <div class="row" id="row_rnd">
+            <div class="articleshowcol">
+                <div class="articleshowrow" id="row_rnd">
                     <h4>Article type: {{ $article->article_type }}</h4>
                 </div>
-                <div class="row" id="row_btw">
+                <div class="articleshowrow" id="row_btw">
                     <h4>Page count: {{ $article->page_count }}</h4>
                 </div>
-                <div class="row" id="row_rnd">
+                <div class="articleshowrow" id="row_rnd">
                     <h4>State: {{ $article->state }}</h4>
                 </div>
             </div>
             <div class="card-body">
                 <h4>Note: {{ $article->note }}</h4>
             </div>
-            <div class="col">
-                <div class="row" id="row_rnd">
+            <div class="articleshowcol2">
+                <div class="articleshowrow" id="row_rnd">
                     <h4>Lang: {{ $article->language }}</h4>
                 </div>
-                <div class="row" id="row_btw">
+                <div class="articleshowrow" id="row_btw">
                     <h4>DOI: {{ $article->doi }}</h4>
                 </div>
-                <div class="row" id="row_rnd">
+                <div class="articleshowrow" id="row_rnd">
                     <h4>Related url: {{ $article->related_url }}</h4>
                 </div>
         </div>
             <tr>
-                <td>
-                    <x-button.magic class="btn-warning"
-                                    :route="route('articles.edit', [$article])">
-                        Edit
-                    </x-button.magic>
-                </td>
+
                 <td>
                     <x-button.magic class="btn-danger" :route="route('articles.destroy', [$article])"
                                     confirm="Are you sure? This can not be undone!">Delete
