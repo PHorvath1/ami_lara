8bf8d5278d8cf16c113e43374e8111a8a9f51893
Váraljai Szabolcs <vaszabi97@gmail.com>
2021-07-20 11:01:40 +0200

    Updated article_show.blade

--- src/resources/views/pages/articles/show.blade.php
+++ src/resources/views/pages/articles/show.blade.php
@@ -1,18 +1,45 @@
 @extends('layouts.app')
 
+@php
+    /**
+    * @var \App\Models\Article $article
+    */
+@endphp
+
 @section('content')
-    <div class="container my-2 my-lg-5">
-        <table class="table-responsive mx-auto">
-            <thead>
-            <tr>
-                <th colspan="2" class="h3">{{ $article->name }}</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr>
-                <th scope="row" class="text-end p-1">Registered at:</th>
-                <td class="p-1">{{ $article->created_at }}</td>
-            </tr>
+    <div class="container-fluid p-4" id="container_color">
+        <div class="card text-center">
+            <div class="card-header">
+                <h3>Name: {{ $article->name }}</h3>
+            </div>
+            <div class="card-body">
+                <h4>Summary: {{ $article->summary }}</h4>
+            </div>
+            <div class="col">
+                <div class="row" id="row_rnd">
+                    <h4>Article type: {{ $article->article_type }}</h4>
+                </div>
+                <div class="row" id="row_btw">
+                    <h4>Page count: {{ $article->page_count }}</h4>
+                </div>
+                <div class="row" id="row_rnd">
+                    <h4>State: {{ $article->state }}</h4>
+                </div>
+            </div>
+            <div class="card-body">
+                <h4>Note: {{ $article->note }}</h4>
+            </div>
+            <div class="col">
+                <div class="row" id="row_rnd">
+                    <h4>Lang: {{ $article->language }}</h4>
+                </div>
+                <div class="row" id="row_btw">
+                    <h4>DOI: {{ $article->doi }}</h4>
+                </div>
+                <div class="row" id="row_rnd">
+                    <h4>Related url: {{ $article->related_url }}</h4>
+                </div>
+        </div>
             <tr>
                 <td>
                     <x-button.magic class="btn-warning"
