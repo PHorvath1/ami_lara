19c05984a74e9351445cbec84ffea0e090deec29
Csathó Zsolt <csatho.zsolt@gmail.com>
2021-07-21 15:42:36 +0200

    The contributors can click on an edit button, which goes to the Article edit form.

--- src/resources/views/pages/articles/show.blade.php
+++ src/resources/views/pages/articles/show.blade.php
@@ -13,19 +13,31 @@
                 <th scope="row" class="text-end p-1">Registered at:</th>
                 <td class="p-1">{{ $article->created_at }}</td>
             </tr>
-            <tr>
-                <td>
-                    <x-button.magic class="btn-warning"
-                                    :route="route('articles.edit', [$article])">
-                        Edit
-                    </x-button.magic>
-                </td>
-                <td>
-                    <x-button.magic class="btn-danger" :route="route('articles.destroy', [$article])"
-                                    confirm="Are you sure? This can not be undone!">Delete
-                    </x-button.magic>
-                </td>
-            </tr>
+            @php
+            $authors=$article->users;
+            $contains = false;
+            foreach ($authors as $author) {
+                if ($author->id === Auth::user()->id) {
+                    $contains = true;
+                    break;
+                }
+            }
+            @endphp
+            @if(\App\Utils\Bouncer::can('edit', $article) || $contains)
+                <tr>
+                    <td>
+                        <x-button.magic class="btn-warning"
+                                        :route="route('articles.edit', [$article])">
+                            Edit
+                        </x-button.magic>
+                    </td>
+                    <td>
+                        <x-button.magic class="btn-danger" :route="route('articles.destroy', [$article])"
+                                        confirm="Are you sure? This can not be undone!">Delete
+                        </x-button.magic>
+                    </td>
+                </tr>
+            @endif
             </tbody>
         </table>
     </div>
