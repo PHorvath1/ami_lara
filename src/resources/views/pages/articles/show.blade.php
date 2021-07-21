a0cd2f2828d4f5dfa6551d9cae3ca7609d9f7854
Váraljai Szabolcs <vaszabi97@gmail.com>
2021-07-21 15:21:24 +0200

    Added comment section to article_show.blade

--- src/resources/views/pages/articles/show.blade.php
+++ src/resources/views/pages/articles/show.blade.php
@@ -10,39 +10,64 @@
     <div class="container-fluid p-4" id="container_color">
         <div id="cardtextbc" class="card text-center">
             <div id="articletitel" class="card-header">
-                <h3 >Name: {{ $article->name }}</h3>
+                <h4 >Name: {{ $article->name }}</h4>
             </div>
             <div class="card-body">
-                <h4 class="lefty">Summary: {{ $article->summary }}</h4>
+                <h5 class="lefty">Summary: {{ $article->summary }}</h5>
             </div>
             <div class="articleshowcol">
                 <div class="articleshowrow" id="row_rnd">
-                    <h4>Article type: {{ $article->article_type }}</h4>
+                    <h5>Article type: {{ $article->article_type }}</h5>
                 </div>
                 <div class="articleshowrow" id="row_btw">
-                    <h4>Page count: {{ $article->page_count }}</h4>
+                    <h5>Page count: {{ $article->page_count }}</h5>
                 </div>
                 <div class="articleshowrow" id="row_rnd">
-                    <h4>State: {{ $article->state }}</h4>
+                    <h5>State: {{ $article->state }}</h5>
                 </div>
             </div>
             <div class="card-body">
-                <h4 class="lefty">Note: {{ $article->note }}</h4>
+                <h5 class="lefty">Note: {{ $article->note }}</h5>
             </div>
             <div class="articleshowcol2">
                 <div class="articleshowrow" id="row_rnd">
-                    <h4>Lang: {{ $article->language }}</h4>
+                    <h5>Lang: {{ $article->language }}</h5>
                 </div>
                 <div class="articleshowrow" id="row_btw">
-                    <h4>DOI: {{ $article->doi }}</h4>
+                    <h5>DOI: {{ $article->doi }}</h5>
                 </div>
                 <div class="articleshowrow" id="row_rnd">
-                    <h4>Related url: {{ $article->related_url }}</h4>
+                    <h5>Related url: {{ $article->related_url }}</h5>
                 </div>
         </div>
             <div class="pdfdownload">
                 <button id="pdfdownloadbtn" class="btn btn-danger" type="button">Download pdf</button>
             </div>
-
+        @can('see-revisions')
+        @endcan
     </div>
+        <div class="container-fluid p-4" id="comm_container_c">
+            <div class="col">
+                <div class="row">
+                    <div class="comment">
+                        <form>
+                            <textarea id="title" type="text "rows="2" cols="60" placeholder="Write a comment......"></textarea>
+                            <input type="submit" value="Post" onclick="insert()" style="width:100px;" />
+                        </form>
+                    </div>
+                </div>
+                @foreach($revisions as $r)
+                    @if($r->article_id === $article->id)
+                        @foreach($r->comments as $comment)
+                            <div class="row_comment">
+                                <div>{{$users->find($comment->user_id)->name }}</div>
+                                <div class="text-muted">{{$comment->created_at}}</div>
+                                <div>{{$comment->content}}</div>
+                                <div class="row_comment_border"></div>
+                            </div>
+                        @endforeach
+                    @endif
+                @endforeach
+            </div>
+        </div>
 @endsection
