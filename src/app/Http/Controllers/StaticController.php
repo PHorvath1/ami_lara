899e89dda9e9a942f46869901bfc2f8a21d7fb18
PHorvath1 <hpeti291@gmail.com>
2021-07-21 13:17:11 +0200

    Documented methods in StaticController

--- src/app/Http/Controllers/StaticController.php
+++ src/app/Http/Controllers/StaticController.php
@@ -8,22 +8,45 @@ use Illuminate\Contracts\View\View;
 
 class StaticController extends Controller
 {
+    /**
+     * Redirects the user to the home page
+     * @return Factory|View|Application
+     */
     public function home(): Factory|View|Application
     {
         return view('pages.home');
     }
 
+    /**
+     * Redirects the user to the test page
+     * @return Application|Factory|View
+     */
     public function test(){
         return view('pages.test');
     }
+
+    /**
+     * Redirects the user to the about page
+     * @return Application|Factory|View
+     */
     public function about(): Factory|View|Application
     {
         return view('pages.about');
     }
+
+    /**
+     * Redirects the user to the content page
+     * @return Application|Factory|View
+     */
     public function content(): Factory|View|Application
     {
         return view('pages.content');
     }
+
+    /**
+     * Redirects the user to the submissions page
+     * @return Application|Factory|View
+     */
     public function submissions(): Factory|View|Application
     {
         return view('pages.submissions');
