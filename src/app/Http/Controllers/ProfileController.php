e64119cb621fa7b5825ca0b9e8620fef557b1b1b
PHorvath1 <hpeti291@gmail.com>
2021-07-21 13:16:40 +0200

    Documented methods in ProfileController

--- src/app/Http/Controllers/ProfileController.php
+++ src/app/Http/Controllers/ProfileController.php
@@ -10,11 +10,19 @@ use Illuminate\Support\Facades\Auth;
 
 class ProfileController extends Controller
 {
+    /**
+     * Redirects the user to it's profile page
+     * @return Factory|View|Application|RedirectResponse
+     */
     public function profile(): Factory|View|Application|RedirectResponse
     {
         return view('pages.users.show', ['user' => Auth::user()]);
     }
 
+    /**
+     * Redirects the user to it's profile editor form
+     * @return Factory|View|Application|RedirectResponse
+     */
     public function editProfile(): Factory|View|Application|RedirectResponse
     {
         return view('pages.users.form', ['user' => Auth::user()]);
