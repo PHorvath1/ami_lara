2ea14b370caaa1409fa704639ab3328de905a8d0
Vincze Flórián <vinczef.o@gmail.com>
2021-07-22 15:50:53 +0200

    Content from h5 to paragraph and added classes

--- src/resources/views/pages/articles/show.blade.php
+++ src/resources/views/pages/articles/show.blade.php
@@ -8,12 +8,14 @@
 
 @section('content')
     <div class="container-fluid p-4" id="container_color">
+
         <div id="cardtextbc" class="card text-center">
             <div id="articletitel" class="card-header">
                 <h4 >Name: {{ $article->name }}</h4>
             </div>
-            <div class="card-body">
-                <h5 class="lefty">Summary: {{ $article->summary }}</h5>
+            <div class="card-bodywrap">
+                <h5 class="closer">Summary:</h5>
+                <p class="lefty"> {{ $article->summary }}</p>
             </div>
             <div class="articleshowcol">
                 <div class="articleshowrow" id="row_rnd">
@@ -26,8 +28,9 @@
                     <h5>State: {{ $article->state }}</h5>
                 </div>
             </div>
-            <div class="card-body">
-                <h5 class="lefty">Note: {{ $article->note }}</h5>
+            <div class="card-bodywrap">
+                <h5 class="closer">Note:</h5>
+                <p class="lefty"> {{ $article->note }}</p>
             </div>
             <div class="articleshowcol2">
                 <div class="articleshowrow" id="row_rnd">
@@ -39,43 +42,46 @@
                 <div class="articleshowrow" id="row_rnd">
                     <h5>Related url: {{ $article->related_url }}</h5>
                 </div>
-        </div>
+            </div>
             <div class="pdfdownload">
                 <button id="pdfdownloadbtn" class="btn btn-danger" type="button">Download pdf</button>
+                <div class="rightfloat">
+                    @php
+                        $authors=$article->users;
+                        $contains = false;
+                        foreach ($authors as $author) {
+                            if ($author->id === Auth::user()->id) {
+                                $contains = true;
+                                break;
+                            }
+                        }
+                    @endphp
+                    @if(\App\Utils\Bouncer::can('edit', $article) || $contains)
+
+                        <div class="btnd">
+                            <x-button.magic class="btn-warning"
+                                            :route="route('articles.edit', [$article])">
+                                Edit
+                            </x-button.magic>
+                        </div>
+                        <div class="btnd">
+                            <x-button.magic class="btn-danger" :route="route('articles.destroy', [$article])"
+                                            confirm="Are you sure? This can not be undone!">Delete
+                            </x-button.magic>
+                        </div>
+                    @endif
+                </div>
             </div>
-        @php
-            $authors=$article->users;
-            $contains = false;
-            foreach ($authors as $author) {
-                if ($author->id === Auth::user()->id) {
-                    $contains = true;
-                    break;
-                }
-            }
-            @endphp
-            @if(\App\Utils\Bouncer::can('edit', $article) || $contains)
-                <tr>
-                    <td>
-                        <x-button.magic class="btn-warning"
-                                        :route="route('articles.edit', [$article])">
-                            Edit
-                        </x-button.magic>
-                    </td>
-                    <td>
-                        <x-button.magic class="btn-danger" :route="route('articles.destroy', [$article])"
-                                        confirm="Are you sure? This can not be undone!">Delete
-                        </x-button.magic>
-                    </td>
-                </tr>
-            @endif
-            </div>
+
+        </div>
+
         <div class="container-fluid p-4" id="comm_container_c">
             <div class="col">
                 <div class="row">
                     <div class="comment">
-                        <form>
+                        <form class="commentfrom">
                             <textarea id="title" type="text "rows="2" cols="60" placeholder="Write a comment......"></textarea>
-                            <input type="submit" value="Post" onclick="insert()" style="width:100px;" />
+                            <input  type="submit" value="Post" onclick="insert()" class="commentbtn" />
                         </form>
                     </div>
                 </div>
@@ -93,4 +99,8 @@
                 @endforeach
             </div>
         </div>
+
+    </div>
+
+
 @endsection
