76d1d5aebc4c1085131e1f6308ad30a4c6b56774
HorKrisz <horvathkrisz00@gmail.com>
2021-07-23 11:29:19 +0200

    Implemented date-range picker in Article index page

--- src/resources/views/pages/articles/index.blade.php
+++ src/resources/views/pages/articles/index.blade.php
@@ -3,6 +3,9 @@
 @push('pre-js')
     {{--    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>--}}
     {{--    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.js"></script>--}}
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
 @endpush
 
 @section('content')
@@ -14,14 +17,14 @@
                         <button id="searchbtn" class="btn btn-primary d-flex d-sm-flex d-md-flex d-lg-flex d-xl-flex justify-content-center align-items-center justify-content-sm-center align-items-sm-center justify-content-md-center align-items-md-center justify-content-lg-center align-items-lg-center justify-content-xl-center align-items-xl-center" type="button"><i class="fas fa-search"></i></button></div>
                     <div class="dropdown">
                         <button class="btn rounded dropdown-toggle btn-advanced-search" id="btn-advanced-search" type="button" data-bs-toggle="dropdown" aria-expanded="false">Advanced search</button>
-                        <ul class="dropdown-menu" aria-labelledby="btn-advanced-search">
-                            <li><label for="author" class="m-1">Author:</label></li>
-                            <li><input type="text" id="author" class="dropdown-item" name="author-search" placeholder="Author"></li>
-                            <li><label for="year" class="m-1">Year:</label></li>
-                            <li><input type="text" id="year" class="dropdown-item" name="year-search" placeholder="Year"></li>
-                            <li><label for="category" class="m-1">Category:</label></li>
-                            <li><input type="text" id="category" class="dropdown-item" name="category-search" placeholder="Categories"></li>
-                            <li><button class="btn btn-primary rounded" name="btn-filter" type="submit">Filter search</button></li>
+                        <ul class="dropdown-menu advanced-search-dropdown" aria-labelledby="btn-advanced-search">
+                            <li><label for="author" class="m-1">Contributor:</label>
+                            <input type="text" id="author" class="dropdown-item" name="contributor-search" placeholder="Contributor"></li>
+                            <li><label for="date" class="m-1">Date:</label>
+                            <input type="text" id="date" class="dropdown-item" name="date-search" value="01/01/2020 - 01/01/2020"></li>
+                            <li><label for="category" class="m-1">Categories:</label>
+                            <input type="text" id="category" class="dropdown-item" name="category-search" placeholder="Categories"></li>
+                            <li><button class="btn btn-primary rounded float-lg-end" name="btn-filter" type="submit">Filter search</button></li>
                         </ul>
                     </div>
                 </form>
@@ -48,4 +51,12 @@
             </div>
         </div>
     </div>
+
+    <script>
+        $(function() {
+            $('input[name="date-search"]').daterangepicker({
+                opens: 'left'
+            });
+        });
+    </script>
 @endsection
