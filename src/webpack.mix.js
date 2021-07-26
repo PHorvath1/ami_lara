4f7e2230719b1d3212b6594d515b4b6dd6aeaad3
Csathó Zsolt <csatho.zsolt@gmail.com>
2021-07-26 14:11:33 +0200

    Supplemented the installation with main and basic views, and basicroutes

--- src/webpack.mix.js
+++ src/webpack.mix.js
@@ -1,16 +1,16 @@
 const mix = require('laravel-mix');
-const glob = require('glob');
 
-//<editor-fold desc="Pattern matching for globify function">
-const files = pattern => glob.sync(pattern, { cwd: 'resources/sass' });
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
 
-const globify = (pattern, out, mixFunctionName) => {
-    files(pattern).forEach((path) => {
-        mix[mixFunctionName](`resources/sass/${path}`, out);
-    })
-};
-//</editor-fold>
-
-mix.sass('resources/sass/app.sass', 'public/css/app.css');
-globify('**/mix_*.sass', 'public/css', 'sass');
-mix.js('resources/js/*.js', 'public/js/app.js');
+mix.js('resources/js/app.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css')
+    .sourceMaps();
