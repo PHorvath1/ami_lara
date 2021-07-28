e9d5bdbad3383d70893939ca36e7fea79fb9554d
Csathó Zsolt <csatho.zsolt@gmail.com>
2021-07-28 13:27:34 +0200

    Wrote the first 10 articles on the admin dasboard page

--- src/app/Http/Controllers/Admin/StaticAdminController.php
+++ src/app/Http/Controllers/Admin/StaticAdminController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Article;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
@@ -12,6 +13,6 @@ class StaticAdminController extends Controller
 {
     public function dashboard(): Factory|View|Application
     {
-        return view('pages.admin.dashboard');
+        return view('pages.admin.dashboard')->with('articles', Article::latest()->limit(10)->get());
     }
 }
--- src/resources/views/pages/admin/dashboard.blade.php
+++ src/resources/views/pages/admin/dashboard.blade.php
@@ -19,12 +19,35 @@
                                           theme="teal" url="#" url-text="View latest articles"/>
                 </div>
                 <div class="col-6 col-sm">
-                    <x-adminlte-small-box title="333" text="Article count" icon="fas fa-poll text-dark"
+                    <x-adminlte-small-box title="{{$articles->count()}}" text="Article count" icon="fas fa-poll text-dark"
                                           theme="warning" url="#" url-text="View all articles"/>
                 </div>
             </div>
         </div>
     </x-card>
+        <div class="card">
+            <div class="card-header ui-sortable-handle" style="cursor: move;">
+                <h3 class="card-title">
+                    <i class="ion ion-clipboard mr-1"></i>
+                    Last 10 articles
+                </h3>
+            </div>
+            <div class="card-body">
+                <ul class="todo-list ui-sortable" data-widget="todo-list">
+                    @php /** @var \App\Models\Article $article */ @endphp
+                    @foreach($articles as $article)
+                        <li>
+                            <span class="text">{{$article->name}}</span>
+                            <small class="badge badge-success"><i class="far fa-clock"></i>
+                                {{$article->created_at->diffForHumans() }}
+                                {{-- TODO: Fix carbon parse --}}
+                            </small>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
+
 @stop
 
 @section('css')
