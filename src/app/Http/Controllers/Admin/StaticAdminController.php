94fe1bdfdf58d9879c90d47ec437cd1abf77f3ae
PHorvath1 <hpeti291@gmail.com>
2021-07-29 08:47:57 +0200

    Removed unused package from StaticAdminController

--- src/app/Http/Controllers/Admin/StaticAdminController.php
+++ src/app/Http/Controllers/Admin/StaticAdminController.php
@@ -7,7 +7,6 @@ use App\Models\Article;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
-use Illuminate\Http\Request;
 
 class StaticAdminController extends Controller
 {
