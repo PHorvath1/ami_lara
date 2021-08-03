152e8db45be4eaf7c408dd66ebaf2ff53c0d682d
Csathó Zsolt <csatho.zsolt@gmail.com>
2021-08-03 11:18:38 +0200

    Fixed database seeder order issue

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -9,10 +9,7 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        foreach (new DirectoryIterator(__DIR__) as $fileInfo) {
-            if($fileInfo->isDot()) continue;
-            if($fileInfo->getFilename() === 'DatabaseSeeder.php') continue;
-            $this->call('Database\Seeders\\' . \Str::before($fileInfo->getFileName(), '.php'));
-        }
+        $this->call(UserSeeder::class);
+        $this->call(BouncerSeeder::class);
     }
 }
