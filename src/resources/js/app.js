e18835065d305c2b1066fa740623a13a83da7b5e
Váraljai Szabolcs <vaszabi97@gmail.com>
2021-08-03 16:05:11 +0200

    Implemented Sass files

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,7 +1,9 @@
-import 'bootstrap'
-
+//import 'bootstrap'
+window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js');
 window._ = require('lodash')
 window.$ = window.jQuery = require('jquery')
 window.axios = require('axios')
 window.toastr = require('toastr')
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
+// noinspection JSValidateTypes
+window.moment = require('moment');
