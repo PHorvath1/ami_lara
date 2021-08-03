9d1cf2a22defa139ed40b038a668b6248f69db49
HorKrisz <horvathkrisz00@gmail.com>
2021-08-03 11:07:50 +0200

    Added routes to admin.php and web.php

--- src/routes/web.php
+++ src/routes/web.php
@@ -9,9 +9,19 @@ use Laravel\Fortify\Http\Controllers\AuthenticatedSessionController;
 Route::get('/', [StaticController::class, 'Welcome'])->name('root');
 
 Route::group(['middleware' => 'auth'], function () {
-    Route::get('/home', [StaticController::class, 'Home'])->name('home');
+    Route::get('/home', [StaticController::class, 'home'])->name('home');
     Route::get('/profile', [ProfileController::class, 'profile'])->name('user.profile');
     Route::get('/profile/edit', [ProfileController::class, 'editProfile'])->name('user.profile.edit');
     Route::get('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('signout');
-    Route::resource('users', UserAdminController::class);
+
+    Route::resource('users', UserController::class);
+    Route::resource('articles', ArticleController::class);
+    Route::resource('volumes', VolumeController::class);
+    Route::post('articles/{article}', [CommentController::class, 'store']) -> name('comment.store');
+    Route::post('/auth/{user}/assign_role/{role}', [BouncerController::class, 'assign_role'])->name('user.role.assign_role');
+    Route::post('/auth/{user}/unassign_role/{role}', [BouncerController::class, 'unassign_role'])->name('user.role.unassign_role');
+    Route::get('/download/{fileName}', [DownloadFileController::class, 'download'])->name('download');
 });
+    Route::get('/about', [StaticController::class, 'about'])->name('about');
+    Route::get('/submissions', [StaticController::class, 'submissions'])->name('submissions');
+    Route::get('/content', [StaticController::class, 'content'])->name('content');
