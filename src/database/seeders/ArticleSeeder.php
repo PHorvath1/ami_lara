61ba94ee2594672bc7426f95390617dd91ba9442
HorKrisz <horvathkrisz00@gmail.com>
2021-08-03 14:08:14 +0200

    Implemented Article seeder

---
+++ src/database/seeders/ArticleSeeder.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Article;
+use Illuminate\Database\Seeder;
+
+class ArticleSeeder extends Seeder
+{
+    public function run(): void
+    {
+        Article::factory(50)->create();
+    }
+}
