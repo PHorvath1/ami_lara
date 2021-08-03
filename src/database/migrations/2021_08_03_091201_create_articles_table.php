83f9bb4bf9d6f68ef5808d33abbbe7960e857a18
HorKrisz <horvathkrisz00@gmail.com>
2021-08-03 11:30:18 +0200

    Implemented Article migration

---
+++ src/database/migrations/2021_08_03_091201_create_articles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->uuid('user_id');
+            $table->uuid('editor_id')->default(null);
+            $table->string('title');
+            $table->text('abstract');
+            $table->tinyInteger('article_state');
+            $table->integer('page_count')->default(null);
+            $table->text('note')->default(null);
+            $table->string('language')->default('en');
+            $table->string('doi')->default(null);
+            $table->text('source')->default(null);
+            $table->id('type_id');
+            $table->string('latex_path');
+            $table->timestamp('deleted_at')->default(null);
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+}
