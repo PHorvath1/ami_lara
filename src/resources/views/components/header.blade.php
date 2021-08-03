5bf342caa5fe4cf36d179159fcf3d94885d3aed1
Váraljai Szabolcs <vaszabi97@gmail.com>
2021-08-03 14:48:20 +0200

    Implemented Components, auth and pages blade files

---
+++ src/resources/views/components/header.blade.php
@@ -0,0 +1,16 @@
+<header>
+    <div class="row" id="mainrow">
+        <div class="col" id="col1"><img src="{{asset('img/header/summa.png')}}" id="img1"></div>
+        <div class="col" id="col2">
+            <div class="row" id="row1">
+                <div class="col d-flex d-sm-flex d-md-flex justify-content-start align-items-center justify-content-sm-start align-items-sm-center justify-content-md-start align-items-md-center" id="col3"><span class="name">Annales Mathematicae</span></div>
+            </div>
+
+            <div class="row" id="row2">
+                <div class="col" id="col3">
+                    <span class="et">ET&nbsp;</span><span class="name">Informaticae</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
--- src/resources/views/components/nav/search.blade.php
+++ src/resources/views/components/nav/search.blade.php
@@ -1,4 +1,6 @@
-<form class="d-flex btn-group">
-    <input class="form-control" type="text" placeholder="Search" aria-label="Search">
-    <button class="btn btn-primary"><i class="fas fa-search"></i></button>
+
+<form id="searchform" class="d-lg-flex me-auto justify-content-lg-end search-form" target="_self">
+    <div class="d-flex justify-content-lg-end"><input class="border rounded form-control search-field" type="search" id="search-field" name="search" >
+        <button id="searchbtn" class="btn btn-primary d-flex d-sm-flex d-md-flex d-lg-flex d-xl-flex justify-content-center align-items-center justify-content-sm-center align-items-sm-center justify-content-md-center align-items-md-center justify-content-lg-center align-items-lg-center justify-content-xl-center align-items-xl-center" type="button" ><i class="fas fa-search" id="searchbtnico" ></i></button></div>
 </form>
+<button id="bellbtn" class="btn btn-primary d-flex d-sm-flex d-xl-flex justify-content-center align-items-center justify-content-sm-center align-items-sm-center justify-content-xl-center align-items-xl-center" type="button" ><i class="fas fa-bell" id="noticitationbtnico" ></i></button>
