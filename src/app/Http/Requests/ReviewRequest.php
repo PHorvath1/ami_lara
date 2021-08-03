e8d7d5d2c0f7938e1a3354919e97c995f489a91e
HorKrisz <horvathkrisz00@gmail.com>
2021-08-03 15:34:12 +0200

    Implemented Review request

---
+++ src/app/Http/Requests/ReviewRequest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ReviewRequest extends ExtendedFormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'content' => [ 'required' ],
+            'state' => []
+        ];
+    }
+}
