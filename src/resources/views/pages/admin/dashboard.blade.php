4508e5070242e4e5073bd556706503c9c4c95172
Csathó Zsolt <csatho.zsolt@gmail.com>
2021-08-05 08:59:28 +0200

    Designed 3 different card

--- src/resources/views/pages/admin/dashboard.blade.php
+++ src/resources/views/pages/admin/dashboard.blade.php
@@ -5,7 +5,7 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-6 col-sm">
-                    <div class="card" >
+                    <div class="card bg-success" >
                         <div class="card-header">
                             <h3 class="card-title">Users Registered</h3>
                             <div class="card-tools">
@@ -30,32 +30,19 @@
                 </div>
                 </div>
                 <div class="col-6 col-sm">
-                    <div class="card">
-                        <div class="card-header">
-                            <h3 class="card-title">Latest articles</h3>
-                            <div class="card-tools">
-                                <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-                                    <i class="fas fa-minus"></i>
-                                </button>
-                                <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
-                                    <i class="fas fa-times"></i>
-                                </button>
-                            </div>
+                    <<div class="small-box bg-warning">
+                        <div class="inner">
+                            <h3>111</h3>
+                            <p>Articles</p>
                         </div>
-                <div class="card-body">
-                    <a href="#">
-                        View latest articles
-                    </a>
-                </div>
-                <!-- /.card-body -->
-                <div class="card-footer">
-                    Footer
-                </div>
-                <!-- /.card-footer-->
-                </div>
+                        <div class="icon">
+                            <i class="ion ion-bag"></i>
+                        </div>
+                        <a href="#" class="small-box-footer">View articles <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
                 </div>
                 <div class="col-6 col-sm">
-                    <div class="card" >
+                    <div class="small-box bg-info" >
                         <div class="card-header">
                             <h3 class="card-title">Article count</h3>
                             <div class="card-tools">
@@ -68,8 +55,8 @@
                             </div>
                         </div>
                     <div class="card-body">
-                        <a href="#">
-                        Start creating your amazing application!
+                        <a href="#" class="nav-link">
+                            View articles
                         </a>
                     </div>
                     <!-- /.card-body -->
