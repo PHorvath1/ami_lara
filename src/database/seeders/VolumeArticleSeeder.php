7057ff4bfde57b717fc8cfe963b49640f0b80cf1
PHorvath1 <hpeti291@gmail.com>
2021-08-05 10:29:46 +0200

    Fixed Volume Article seeder

--- src/database/seeders/VolumeArticleSeeder.php
+++ src/database/seeders/VolumeArticleSeeder.php
@@ -9,10 +9,10 @@ use Illuminate\Database\Seeder;
 
 class VolumeArticleSeeder extends Seeder
 {
-    public function up(): void
+    public function run(): void
     {
         Article::all()->each(function (Article $article) {
-            $article->volumes()->attach(Volume::random());
+            $article->volumes()->get()->add(Volume::random());
             $article->save();
         });
     }
