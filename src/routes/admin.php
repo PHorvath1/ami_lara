f1289303fb3417a7e52de8f47024844da50aad6d
PHorvath1 <hpeti291@gmail.com>
2021-08-06 11:59:09 +0200

    Added missing imports to admin routes

--- src/routes/admin.php
+++ src/routes/admin.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Admin\StaticAdminController;
+use App\Http\Controllers\UserAdminController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', [StaticAdminController::class, 'dashboard'])->name('dashboard');
 Route::resource('users', UserAdminController::class);
