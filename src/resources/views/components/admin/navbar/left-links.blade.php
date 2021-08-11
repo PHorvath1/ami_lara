774a9a8c8dd4262ce7d887de2baade2f0b374c75
Csathó Zsolt <csatho.zsolt@gmail.com>
2021-08-11 10:12:16 +0200

    Edited menus on the admin page

--- src/resources/views/components/admin/navbar/left-links.blade.php
+++ src/resources/views/components/admin/navbar/left-links.blade.php
@@ -6,14 +6,13 @@
         <a href="admin" class="nav-link">Home</a>
     </li>
     <li class="nav-item d-none d-sm-inline-block">
-        <a href="#" data-toggle="modal" data-target=".bd-example-modal-sm" class="nav-link">Contact</a>
+        <a href="#" data-toggle="modal" data-target=".bd-modal-sm" class="nav-link">Contact</a>
     </li>
-    <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
 </ul>
-<div class="modal fade bd-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
+<div class="modal fade bd-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel">
     <div class="modal-dialog modal-sm">
         <div class="modal-content">
-            twtergewth
+            test@test.hu
         </div>
     </div>
 </div>
--- src/resources/views/components/admin/sidebar/menu.blade.php
+++ src/resources/views/components/admin/sidebar/menu.blade.php
@@ -8,23 +8,12 @@
                 <p>Dashboard</p>
             </a>
         </li>
-        <li class="nav-item">
-            <a href="#" class="nav-link">
-                <i class="nav-icon fas fa-th"></i>
-                <p>
-                    Inbox
-                    <i class="right fas fa-angle-left"></i>
-                </p>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a href="#" class="nav-link">
-                <i class="nav-icon fas fa-copy"></i>
-                <p>
-                    Content
-                    <i class="fas fa-angle-left right"></i>
-                </p>
-            </a>
+
+        <x-admin.sidebar.inbox />
+
+        <x-admin.sidebar.content_menu />
+
+        <li class="nav-header">OTHERS</li>
             <ul class="nav nav-treeview">
                 <li class="nav-item">
                     <a href="../layout/top-nav.html" class="nav-link">
