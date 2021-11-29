4df24aa63a703b32fc3b7901e91b1932401f64f0
Bozsokaroly <karcsivokxd123@gmail.com>
2021-11-29 21:04:33 +0100

    Implemented ArticleAdminController.store function

--- src/app/Http/Controllers/Admin/ArticleAdminController.php
+++ src/app/Http/Controllers/Admin/ArticleAdminController.php
@@ -2,8 +2,14 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Actions\File\FileManager;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\GuardedController;
+use App\Http\Requests\ArticleCreateRequest;
+use App\Models\Article;
+use App\Models\Revision;
+use App\Providers\CategoryServiceProvider;
+use Brian2694\Toastr\Facades\Toastr;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
@@ -22,9 +28,21 @@ class ArticleAdminController extends GuardedController
         return true;
     }
 
-    public function store(): Factory|View|Application|RedirectResponse
+    public function store(ArticleCreateRequest $request): Factory|View|Application|RedirectResponse
     {
-        return true;
+        $rq = $request->validated();
+        $rqe = $request->except(['categories']);
+        $article = Article::create($rqe);
+
+        $categories = explode(',', $rq['categories']);
+
+        $fm = new FileManager();
+        $pdf = $fm->upload($request, 'pdf', 'articles');
+        //$latex = $fm->upload($request, 'latex', 'articles');
+        CategoryServiceProvider::attachAll($categories, $article);
+        Revision::create(['note' => 'Article submitted','pdf_path' => $pdf, 'article_id' => $article->id]);
+        Toastr::success('New article created');
+        return redirect(route('articles.show', [$article]));
     }
 
     public function show(): Factory|View|Application|RedirectResponse
