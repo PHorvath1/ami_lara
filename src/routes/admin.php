567f39a7601e68d86f9aafc8a881d02d05edea71
tresze <42722314+tresze@users.noreply.github.com>
2021-11-30 11:46:23 +0100

    Fixes in UserAdminController (#209)
    * Fixed path in UserAdminController
    
    * Fixed path of View users button on admin page
    
    * Fixed routing of Users button on admin page sidebar

--- src/routes/admin.php
+++ src/routes/admin.php
@@ -2,9 +2,9 @@
 
 use App\Http\Controllers\Admin\StaticAdminController;
 use App\Http\Controllers\admin\VolumeAdminController;
-use App\Http\Controllers\UserAdminController;
+use App\Http\Controllers\Admin\UserAdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [StaticAdminController::class, 'dashboard'])->name('dashboard');
-Route::resource('users', UserAdminController::class);
+Route::resource('/users', UserAdminController::class);
 Route::resource('/volumes', VolumeAdminController::class);
