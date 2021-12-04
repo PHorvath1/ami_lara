15e5d56b88f9019666bfc703a69d1db729abb07c
RachidAifar <90982692+RachidAifar@users.noreply.github.com>
2021-12-04 18:42:36 +0100

    feat: Implement ArticleAdminController.index function (#215)

--- src/app/Http/Controllers/Admin/ArticleAdminController.php
+++ src/app/Http/Controllers/Admin/ArticleAdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\GuardedController;
 use App\Models\Article;
+use App\Models\User;
 use Brian2694\Toastr\Facades\Toastr;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
@@ -16,7 +17,7 @@ class ArticleAdminController extends GuardedController
 {
     public function index(): Factory|View|Application|RedirectResponse
     {
-        return true;
+        return view('pages.admin.articles.index', ['article' => Article::all()]);
     }
 
     public function create(): Factory|View|Application|RedirectResponse
