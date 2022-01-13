52a7528ae703a9a7a42c38fe4572f0014767e7aa
Fejes Bence <fejesbencegr@gmail.com>
2022-01-13 12:13:02 +0100

    Added role management page route to admin routes

--- src/routes/admin.php
+++ src/routes/admin.php
@@ -4,9 +4,11 @@ use App\Http\Controllers\Admin\StaticAdminController;
 use App\Http\Controllers\admin\VolumeAdminController;
 use App\Http\Controllers\Admin\ArticleAdminController;
 use App\Http\Controllers\Admin\UserAdminController;
+use App\Http\Controllers\Admin\RoleAdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [StaticAdminController::class, 'dashboard'])->name('dashboard');
 Route::resource('/users', UserAdminController::class);
 Route::resource('/volumes', VolumeAdminController::class);
 Route::resource('/articles', ArticleAdminController::class);
+Route::resource('/roles', RoleAdminController::class);
