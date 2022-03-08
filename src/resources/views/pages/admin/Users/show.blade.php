7b72d903766136b95eb09512436071125d801c0f
tresze <42722314+tresze@users.noreply.github.com>
2022-03-08 02:39:55 +0100

    Design rework done in the show file of admin/users page
    This commit addresses one of the requested changes on issue #226.

--- src/resources/views/pages/admin/Users/show.blade.php
+++ src/resources/views/pages/admin/Users/show.blade.php
@@ -1,58 +1,24 @@
 @extends('layouts.admin')
 
 @section('content')
-    <div class="containerusershow">
+    @php
+        /**
+        * @var User $user
+        */
+        use App\Models\User;
+    @endphp
 
-
-        <div class="colgrid">
-            <div class="row profrow">
-                <h3 class="h3title">{{ $user->name }}</h3>
-                <span class="spanbold">
-                   Email:
-                   <p class="cleantext">{{ $user->email }}</p>
-               </span>
-                <span class="spanbold">
-                   Registered at:
-                   <p class="cleantext">{{ $user->created_at }}</p>
-               </span>
+    <div class="container-fluid p-4 volume_container_color">
+        <div class="card text-center">
+            <div class="card-header">
+                <h3>Name: {{ $user->name }}</h3>
             </div>
-            <div class="rowgrid topmarginrowgrid">
-                <div class="rowgrid rowgridcolor">
-                    <h3 class="h3title">Roles:</h3>
-                </div>
-                <div class="row">
-                    <div class="colgrid">
-                        <span class="spanbold">
-                            Role name:
-                            @foreach($user->roles as $role)
-                                <p class="cleantext">{{ $role->title }}</p>
-                            @endforeach
-                        </span>
-                        <div class="buttonwraper">
-                            <x-button.magic class="btn-warning"
-                                            :route="Auth::id() === $user->id ? route('user.profile.edit') : route('users.edit', [$user])">
-                                Edit
-                            </x-button.magic>
-                        </div>
-                    </div>
-                    <div class="colgrid">
-                        <span class="spanbold">
-                            Provides:
-                            @foreach($user->roles as $role)
-                                <p class="cleantext">{{ $role->abilities->implode('title', ', ') }}</p>
-                            @endforeach
-                        </span>
-                        <x-button.magic class="btn-danger" :route="route('users.destroy', [$user])"
-                                        confirm="Are you sure? This can not be undone!">Delete
-                        </x-button.magic>
-                    </div>
-                </div>
-                <div class="row">
-
-                </div>
+            <div class="card-body">
+                <h4>Email: {{ $user->email }}</h4>
+            </div>
+            <div class="card-footer text-muted">
+                Registration date: {{ $user->created_at }}
             </div>
         </div>
-
-
     </div>
 @endsection
